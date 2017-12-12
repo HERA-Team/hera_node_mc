98bcd575ee394a36d791e833909f55cf435fa7ac
Zara <zuhraa470@gmail.com>
2017-12-12 15:32:24 -0800

    Adding argparser to udpReceiver for arduino ip input, not yet working, weird syntax error

--- udpReceiver.py
+++ udpReceiver.py
@@ -1,3 +1,17 @@
 import udpReceiverClass
+import argparse
+
+parser = argparse.ArgumentParser(description = 'This scripts instantiates the udpReceiverClass and kicks off the receiveUDP with Arduino IP as argument',
+                                    formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('ip_address', dest = 'ip_addr', help = 'Specify the Arduino IP address')
+
+args = parser.parse_args()
+ 
+
+
+
+
+
+
 u = udpReceiverClass.UdpClient()
-u.receiveUDP('10.1.1.200')
+u.receiveUDP(args.ip_addr)
