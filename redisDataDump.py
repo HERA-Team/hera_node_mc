2120851031a8abdd7c8a70f17438fc96b7e2b859
Zara <zuhraa470@gmail.com>
2017-12-12 17:02:47 -0800

    added argparse

--- redisDataDump.py
+++ redisDataDump.py
@@ -1,7 +1,16 @@
 import os 
 import time
+import argparse
+
+parser = argparse.ArgumentParser(description = 'This scripts dumps the contents of the Redis database into a text file every x seconds - default is 300.',
+                                    formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('file_name', action = 'store', help = 'Specify the file name to dump Redis data to i.e. datasetMMDDYY.txt')
+parser.add_argument('-t', action = 'store', dest = 'interval', help = 'Specify the time interval, in seconds, for data collection')
+
+args = parser.parse_args()
+
 while True:
-    os.system("redis-cli hgetall status:node:0 >> dataset112117.txt")
+    os.system("redis-cli hgetall status:node:0 >> %s"%args.file_name)
     time.sleep(300)
 
     
