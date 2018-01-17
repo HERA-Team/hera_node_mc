9a30ab007d704ab0b9e26eda6b14b599c81eb385
Zara <zuhraa470@gmail.com>
2018-01-16 16:48:33 -0800

    udpSerial formatting test

---
+++ arduino-mk/mc_arduino/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='monitor-control',
+    version='0.1',
+    py_modules=['udpReceiverClass',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
