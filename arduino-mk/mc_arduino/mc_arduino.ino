4638a6d32296e1f4855e295b0891879eb3456c34
reeveress <zuhraa470@gmail.com>
2018-02-08 15:19:22 -0800

    Initialized nodeID and node_metadata to -99

--- arduino-mk/mc_arduino/mc_arduino.ino
+++ arduino-mk/mc_arduino/mc_arduino.ino
@@ -101,8 +101,8 @@ Adafruit_HTU21DF htu = Adafruit_HTU21DF();
 // Status struct will be sent in a UDP packet 
 struct status {
 	unsigned long cpu_uptime_ms = -99;  // Arduino uptime since last reset
-	byte  nodeID=0;
-	byte  nodeID_meta=0;                  // Node ID as give by the digi I/O card hanging from PCB
+	byte  nodeID = -99;                 // Digi I/O bits 0 through 7, first 6 give the node ID
+	byte  nodeID_metadata = -99;	    // Digi I/O bits 8 through 15, bit 8 specifies if node is a prototype or production 	    
 	float mcpTempTop = -99;             // Top temperature sensor value
 	float mcpTempMid = -99;             // Mid temperature sensor value
 	float htuTemp = -99;                // HTU21D sensor temperature value
@@ -231,7 +231,7 @@ void setup() {
 			serialUdp(String(statusStruct.nodeID));
       		}
       		for (int i=8; i<16; i++){
-			statusStruct.nodeID_meta |= io.digitalRead(i) << i;
+			statusStruct.nodeID_metadata |= io.digitalRead(i) << i;
 		}
 	}
 	else {
