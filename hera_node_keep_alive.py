9dc4e942a524e5ca182d93b1b598eeb1bf63fa62
reeveress <zuhraa470@gmail.com>
2018-02-13 15:02:35 -0800

    got rid of passing nodes to keep alive, instead it gets the nodes from redis status keys and pokes those

--- hera_node_keep_alive.py
+++ hera_node_keep_alive.py
@@ -1,32 +1,27 @@
 import time
-import argparse 
 import redis
 import udpSender
 import sys
 
 """
-This script pokes Arduino every 3 seconds to ensure Arduino's connectivity to the server.
-It also checks the Redis Database flags set from the nodeControlClass and for commands and
-sends those commands to the Arduino.
-Takes in the IP address of the Arduino to poke as a string and the corresponding node ID as an int. 
+This script pokes Arduino every second to ensure Arduino's connectivity to the server.
+It pokes only the nodes that have Redis status keys i.e. status:node:x where x is the node ID
+set by the I2C digital I/O cards plugged into PCBs.  
 """
-
-parser = argparse.ArgumentParser(description = 'This script continuously pokes an Arduinos inside the nodes specified by the node argument. It also checks for command flag change inside the Redis database that are set through the nodeControlClass.', formatter_class = argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('node', metavar = 'node', type=int, nargs='+', help = 'List of integer node IDs.')
-args = parser.parse_args()
-
-r = redis.StrictRedis('localhost')
-
+# Dictionary to hold UdpSender objects for each node.
 s={}
-for node in args.node:
-    ip_addr = r.hget('status:node:%d'%node, 'ip')
-    s['node%d'%node] = udpSender.UdpSender(ip_addr)
-    print(s['node%d'%node])
-
-# Sends poke signal to Arduino every second
+nodes = []
+i = 0
+# Iterates through all keys that start with status: and gets their node IDs and Arduino IP addresses.
+for key in r.scan_iter("status:*"):
+        nodes.append(int(r.hget(key,'node_ID')))
+        s['node%d'%nodes[i]] = udpSender.UdpSender(r.hget(key,'ip'))
+        i += 1
+# Sends poke signal to Arduinos inside nodes every second
 try:
     while True:
-        for node in args.node:
+        for node in nodes:
+            print("Poking node %d"%node)
             s['node%d'%node].poke() 
             # Check if Redis flags were set through the nodeControlClass
             time.sleep(1)
