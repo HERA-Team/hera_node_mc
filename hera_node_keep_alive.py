190b462ec1cafad01b7810157ceda86836bb37fb
reeveress <zuhraa470@gmail.com>
2018-02-15 08:44:40 -0800

    formatting, changed sleep from 1 second to .03

--- hera_node_keep_alive.py
+++ hera_node_keep_alive.py
@@ -1,13 +1,16 @@
+"""
+Pokes Arduino every second to ensure Arduino's connectivity to the server.
+It pokes only the nodes that have Redis status keys i.e. status:node:x where x is the node ID
+set by the I2C digital I/O cards plugged into PCBs.  
+"""
+
 import time
 import redis
 import udpSender
 import sys
 
-"""
-This script pokes Arduino every second to ensure Arduino's connectivity to the server.
-It pokes only the nodes that have Redis status keys i.e. status:node:x where x is the node ID
-set by the I2C digital I/O cards plugged into PCBs.  
-"""
+
+r = redis.StrictRedis()
 # Dictionary to hold UdpSender objects for each node.
 s={}
 nodes = []
@@ -17,14 +20,14 @@ for key in r.scan_iter("status:*"):
         nodes.append(int(r.hget(key,'node_ID')))
         s['node%d'%nodes[i]] = udpSender.UdpSender(r.hget(key,'ip'))
         i += 1
-# Sends poke signal to Arduinos inside nodes every second
+# Sends poke signal to Arduinos inside the nodes
 try:
     while True:
         for node in nodes:
             print("Poking node %d"%node)
             s['node%d'%node].poke() 
-            # Check if Redis flags were set through the nodeControlClass
-            time.sleep(1)
+            r.hset('status:node:%d'%node,'last_poke',time.time())
+            time.sleep(.03)
 except KeyboardInterrupt:
     print('Interrupted')
     sys.exit(0)
