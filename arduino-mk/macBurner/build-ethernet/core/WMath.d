23d439f4746004ce9edef5a7556e2d96611b7a60
reeveress <zuhraa470@gmail.com>
2018-02-22 11:44:03 -0800

    got rid of macBurner

--- arduino-mk/macBurner/build-ethernet/core/WMath.d
+++
@@ -1,2 +0,0 @@
-build-ethernet/core/WMath.o: \
- /home/zara/arduino-1.6.5-r5/hardware/arduino/avr/cores/arduino/WMath.cpp
--- arduino-mk/macBurner/build-ethernet/core/hooks.d
+++
@@ -1,2 +0,0 @@
-build-ethernet/core/hooks.o: \
- /home/zara/arduino-1.6.5-r5/hardware/arduino/avr/cores/arduino/hooks.c
