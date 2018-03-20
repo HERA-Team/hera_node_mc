18c290ceee7174f83d460cd988aad39509fdf979
reeveress <zuhraa470@gmail.com>
2018-03-20 14:36:57 -0700

    setup file and ran python setup.py install

---
+++ build/scripts-2.7/hera_node_get_status.py
@@ -0,0 +1,9 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description = 'This scripts outputs the current node status',
+                                    formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('node',action='store', help='Specify the node ID number (int from 0 to 29) to get the corresponding Redis data')
+args = parser.parse_args()
+
+os.system("redis-cli hgetall status:node:%d"%(int(args.node)))
---
+++ build/scripts-2.7/hera_node_turn_off_sender.py
@@ -0,0 +1,95 @@
+import udpSender
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description = 'Specify the things for PCB to turn off via flags',
+			formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('ip_addr', action = 'store', help = 'Specify the Arduino IP address to send commands to')
+
+parser.add_argument('-r', dest = 'snapRelay', action = 'store_true', default = False,
+			help = 'Use this flag to turn off the snapRelay')
+parser.add_argument('-s', dest = 'snaps', action = 'store_true', default = False,
+			help = 'Use this flag to turn off all the snaps')
+parser.add_argument('-s0', dest = 'snap0', action = 'store_true', default = False,
+			help = 'Use this flag to turn off SNAP 0')
+parser.add_argument('-s1', dest = 'snap1', action = 'store_true', default = False,
+			help = 'Use this flag to turn off SNAP 1')
+parser.add_argument('-s2', dest = 'snap2', action = 'store_true', default = False,
+			help = 'Use this flag to turn off SNAP 2')
+parser.add_argument('-s3', dest = 'snap3', action = 'store_true', default = False,
+			help = 'Use this flag to turn off SNAP 3')
+parser.add_argument('-p', dest = 'pam', action = 'store_true', default = False,
+			help = 'Use this flag to turn off the PAM')
+parser.add_argument('-f', dest = 'fem', action = 'store_true', default = False,
+			help = 'Use this flag to turn off the FEM')
+parser.add_argument('--reset', dest = 'reset', action = 'store_true', default = False,
+			help = 'Use this flag to reset Arduino (turn everything off abruptly')
+args = parser.parse_args()
+
+# Instantiate a UdpSender class object to send commands to Arduino
+s = udpSender.UdpSender(args.ip_addr)
+
+if args.snaps:
+    print("Turning SNAP 0 off")
+    s.power_snap_0('off')
+    time.sleep(1)
+    print("Turning SNAP 1 off")
+    s.power_snap_1('off')
+    time.sleep(1)
+    print("Turning SNAP 2 off")
+    s.power_snap_2('off')
+    time.sleep(1)
+    print("Turning SNAP 3 off")
+    s.power_snap_3('off')
+    time.sleep(1)
+    print("Turning SNAP relay off")
+    s.power_snap_relay('off')
+    time.sleep(.1)
+
+if args.snapRelay:
+    s.power_snap_0('off')
+    time.sleep(1)
+    s.power_snap_1('off')
+    time.sleep(1)
+    s.power_snap_2('off')
+    time.sleep(1)
+    s.power_snap_3('off')
+    time.sleep(1)
+    print("Turning snapRelay off")
+    s.power_snap_relay('off')
+    time.sleep(.1)
+
+if args.snap0:
+    print("Turning SNAP 0 off")
+    s.power_snap_0('off')
+    time.sleep(1)
+
+if args.snap1:
+    print("Turning SNAP 1 off")
+    s.power_snap_1('off')
+    time.sleep(1)
+
+if args.snap2:
+    print("Turning SNAP 2 off")
+    s.power_snap_2('off')
+    time.sleep(1)
+
+if args.snap3:
+    print("Turning SNAP 3 off")
+    s.power_snap_3('off')
+    time.sleep(1)
+
+
+if args.pam:
+    print("Turning PAM off")
+    s.power_pam('off')
+    time.sleep(1)
+
+if args.fem:
+    print("Turning FEM off")
+    s.power_fem('off')
+    time.sleep(1)
+
+if args.reset:
+    print("Resetting Arduino/Turning everything off at once")
+    s.reset()
