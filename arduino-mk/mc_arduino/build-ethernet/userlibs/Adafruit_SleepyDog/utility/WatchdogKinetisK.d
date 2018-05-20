6ceef6ec81666be78bd92526371636ccdb26cbed
reeveress <zuhraa470@gmail.com>
2018-05-20 02:34:07 -0700

    Remove build files

--- arduino-mk/mc_arduino/build-ethernet/userlibs/Adafruit_SleepyDog/utility/WatchdogKinetisK.d
+++
@@ -1,2 +0,0 @@
-build-ethernet/userlibs/Adafruit_SleepyDog/utility/WatchdogKinetisK.o: \
- ../../arduino-mk/libraries/Adafruit_SleepyDog/utility/WatchdogKinetisK.cpp
--- arduino-mk/mc_arduino/build-ethernet/userlibs/Adafruit_SleepyDog/utility/WatchdogKinetisL.d
+++
@@ -1,2 +0,0 @@
-build-ethernet/userlibs/Adafruit_SleepyDog/utility/WatchdogKinetisL.o: \
- ../../arduino-mk/libraries/Adafruit_SleepyDog/utility/WatchdogKinetisL.cpp
