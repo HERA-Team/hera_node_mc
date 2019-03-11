09a1e73bb7d2e2cdffe2a862ee88b4d5a0aa01dd
reeveress <zuhraa470@gmail.com>
2019-03-11 22:33:46 +0000

    Add versioning

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 from setuptools import setup, find_packages
 import glob
+import os
+
+ver = '0.0.1'
+try:
+    import subprocess
+    ver = ver + '-' + subprocess.check_output(['git', 'describe', '--abbrev=8', '--always', '--dirty', '--tags']).strip()
+except:
+    print('Couldn\'t get version from git. Defaulting to %s' % ver)
+
+# Generate a __version__.py file with this version in it
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'monitor-control', 'nodeControl', '__version__.py'), 'w') as fh:
+    fh.write('__version__ = "%s"' % ver)
+with open(os.path.join(here, 'backend', 'udpSender', '__version__.py'), 'w') as fh:
+    fh.write('__version__ = "%s"' % ver)
 
 setup(
     name = 'monitor-control',
@@ -28,3 +43,11 @@ setup(
                 ]
     
 )
+
+if ver.endswith("dirty"):
+    print "********************************************"
+    print "* You are installing from a dirty git repo *"
+    print "*      One day you will regret this.       *"
+    print "*                                          *"
+    print "*  Consider cleaning up and reinstalling.  *"
+    print "********************************************"
