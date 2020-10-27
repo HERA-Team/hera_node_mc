2137517006413fba1c8973cf974808e1216dc889
David DeBoer <ddeboer@berkeley.edu>
2020-10-27 10:18:17 -0700

    split out version vs status

--- monitor-control/scripts/hera_service_status.py
+++ monitor-control/scripts/hera_service_status.py
@@ -5,5 +5,7 @@ connection_pool = redis.ConnectionPool(host='redishost', decode_responses=True)
 r = redis.StrictRedis(connection_pool=connection_pool, charset='utf-8')
 
 for key in r.keys():
-    if key.decode().startswith('status:script') or key.decode().startswith('version'):
+    if key.decode().startswith('status:script'):
         print(key.decode(), r.get(key))
+    if key.decode().startswith('version'):
+        print(key.decode(), r.hgetall(key))
