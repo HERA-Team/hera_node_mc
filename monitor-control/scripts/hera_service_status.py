e8b504e7fd57dcd324e4a2ae8f2dc21257e6a701
David DeBoer <ddeboer@berkeley.edu>
2020-10-27 10:34:53 -0700

    udpate status

--- monitor-control/scripts/hera_service_status.py
+++ monitor-control/scripts/hera_service_status.py
@@ -4,8 +4,36 @@ import redis
 connection_pool = redis.ConnectionPool(host='redishost', decode_responses=True)
 r = redis.StrictRedis(connection_pool=connection_pool, charset='utf-8')
 
+expected_services = {'hera_node_keep_alive': {'status': [], 'version': []},
+                     'hera_node_receiver': {'status': [], 'version': []},
+                     'hera_node_cmd_check': {'status': [], 'version': []}}
+
 for key in r.keys():
+    dkey = key.decode()
     if key.decode().startswith('status:script'):
-        print(key.decode(), r.get(key))
+        for this_service in expected_services.keys():
+            if this_service in dkey:
+                expected_services[this_service]['status'] = [dkey, r.get(key)]
+                break
+        else:
+            print("Other service")
+            print('\t', dkey, r.get(key))
     if key.decode().startswith('version'):
-        print(key.decode(), r.hgetall(key))
+        for this_service in expected_services.keys():
+            if this_service in dkey:
+                expected_services[this_service]['version]'] = [dkey, r.hgetall(key)]
+                break
+        else:
+            print("Other service")
+            print('\t', dkey, r.hgetall(key))
+
+for this_service, stat in expected_services.items():
+    print(this_service)
+    if len(stat['version']):
+        print('\tversion: ', stat['version'])
+    else:
+        print('\tversion: N/A')
+    if len(stat['status']):
+        print('\tstatus: ', stat['status'])
+    else:
+        print('\tstatus: not alive')
