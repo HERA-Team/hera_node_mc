33ea6d69a3eb4102f4890b9d6367159cdd095e96
David DeBoer <ddeboer@berkeley.edu>
2020-10-29 22:11:37 -0700

    draft of new node

--- monitor-control/scripts/hera_setup_new_node.py
+++ monitor-control/scripts/hera_setup_new_node.py
@@ -2,10 +2,10 @@
 from __future__ import print_function
 import argparse
 import os
-import time
-import json
 import redis
 import socket
+from node_control import hosts_ethers
+
 
 # These are dumped from hera_mc on October 27, 2020
 RDmap = {'NCMP2': 'RD31', 'NCM02': 'RD46', 'NCM06': 'RD20', 'NCM10': 'RD43', 'NCM13': 'RD18',
@@ -23,7 +23,6 @@ WRmap = {'NCMP2': 'WRA000028', 'NCM03': 'WRA000084', 'NCM06': 'WRA000093', 'NCM0
          'NCM21': 'WRC000153', 'NCM19': 'WRC000154', 'NCMP1': 'WR-len4p0'
          }
 
-
 ap = argparse.ArgumentParser()
 ap.add_argument('node_num', help="Node number (int)", type=int)
 ap.add_argument('ncm', help="Node-control-module (NCM) number (int)", type=int)
@@ -33,87 +32,54 @@ ap.add_argument('snap2', help="SNAP serial number (int)", type=int)
 ap.add_argument('snap3', help="SNAP serial number (int)", type=int)
 ap.add_argument('--snap_rev', help="__Shouldn't need to change__  Rev letter of SNAP (csv-list).  "
                 "If one supplied, it applies to all.", default='C')
-ap.add_argument('--hosts_file', help="__Shouldn't need to change__  Name of hosts file.",
-                default="/etc/hosts")
-ap.add_argument('--dont_reset_dnsmasq', dest='reset_dnsmasq', help="Don't reset the dnsmasq",
+ap.add_argument('--dont-reset-dnsmasq', dest='reset_dnsmasq', help="Don't reset the dnsmasq",
                 action='store_false')
 args = ap.parse_args()
-snap_rev = args.snap_rev.upper().split(",")
-if len(snap_rev) == 1:
-    snap_rev = snap_rev * 4
 
-this_node_num = int(args.node_num)
-snaps = {}
-for i in range(4):
-    snpi = 'heraNode{}Snap{}'.format(this_node_num, i)
-    snaps[snpi] = 'SNP{}{:06d}'.format(snap_rev[i], getattr(args, 'snap{}'.format(i)))
+ncm = 'NCM{:02d}'.format(args.ncm)
 hostname = socket.gethostname()
 if hostname == 'hera-mobile':  # RFI testing machine, so want to write the current node.
     with open('CurrentNode.txt', 'w') as fp:
-        fp.write(this_node_num)
+        fp.write(args.node_num)
 
-print("Rewriting {} with".format(args.hosts_file))
-for loc, snr in snaps.items():
-    print("\t{} --> {}".format(snr, loc))
-backup_host_file = ('{}.backup_etc_hosts{}'
-                    .format(os.path.basename(args.hosts_file), int(time.time())))
-print("Backing up to {}".format(backup_host_file))
-os.system('cp {} {}'.format(args.hosts_file, backup_host_file))
-update_counter = 0
+# Read files and get macs and ips
+hosts = hosts_ethers.HostsEthers('/etc/hosts')
+ethers = hosts_ethers.HostsEthers('/etc/ethers')
 
-with open('rfimacip.json', 'r') as fp:
-    macip = json.load(fp)
-for arduino, info in macip.items():
-    if info['node'] == this_node_num:
-        print("Using arduino {}".format(arduino))
-        break
+# Set up SNAPs
+snap_rev = args.snap_rev.upper().split(",")
+if len(snap_rev) == 1:
+    snap_rev = snap_rev * 4
+snaps = [{} * 4]
+for i in range(4):
+    snaps[i]['node'] = 'heraNode{}Snap{}'.format(args.node_num, i)
+    snaps[i]['snap'] = 'SNP{}{:06d}'.format(snap_rev[i], getattr(args, 'snap{}'.format(i)))
+    snaps[i]['mac'] = ethers[snaps[i]['snap']]
+    snaps[i]['ip'] = hosts[snaps[i]['snap']]
+    hosts.update_id(snaps[i]['ip'], '{}\t{}'.format(snaps[i]['snap'], snaps[i]['node']))
+
+# Set up arduino
+rd = {'rd': 'arduino{}'.format(RDmap[ncm][2:])}
+rd['mac'] = ethers[rd['rd']]
+rd['ip'] = hosts[rd['rd']]
+rd['node'] = 'heraNode{}'.format(args.node_num)
+hosts.update_id(rd['ip'], '{}\t{}'.format(rd['rd'], rd['node']))
+
+# Set up white rabbit
+wr = {'wr': 'wr{}'.format(WRmap[ncm][2:])}
+wr['mac'] = ethers[wr['wr']]
+wr['ip'] = hosts[wr['wr']]
+wr['node'] = 'heraNode{}wr'.format(args.node_num)
+hosts.update_id(wr['ip'], '{}\t{}'.format(wr['wr'], wr['node']))
+
+hosts.rewrite_file()
 
 connection_pool = redis.ConnectionPool(host='redishost', decode_responses=True)
 r = redis.StrictRedis(connection_pool=connection_pool, charset='utf-8')
 rkey = 'status:node:{}'.format(int(args.node_num))
-r.hset(rkey, 'ip', macip[arduino]['ip'])
-r.hset(rkey, 'mac', macip[arduino]['mac'])
-r.hset(rkey, 'node_ID', int(args.node_num))
-
-with open(backup_host_file, 'r') as fpin:
-    with open(args.hosts_file, 'w') as fpout:
-        for line in fpin:
-            # Check overall conditions to not update line.
-            if line[0] == '#' or 'SNP' not in line:
-                fpout.write("{}".format(line))
-                continue
-            data = line.split()
-            if len(data) < 2 or len(data) > 3:
-                fpout.write("{}".format(line))
-                continue
-            # Check if heraNodeXSnapY already present.  If so, remove from line.
-            if len(data) == 3:
-                for loc in snaps.keys():
-                    if loc == data[2]:
-                        print("Removing existing {}".format(line.strip()))
-                        line = '{} {}\n'.format(data[0], data[1])
-                        break
-            # Check if SNPC0000NN and add heraNodeXSnapY if found.
-            for loc, snr in snaps.items():
-                if snr == data[1]:
-                    new_line = '{} {}  {}\n'.format(data[0], data[1], loc)
-                    fpout.write(new_line)
-                    print("{:50s}  -->  {}".format(line.strip(), new_line.strip()))
-                    update_counter += 1
-                    break
-            else:
-                fpout.write("{}".format(line))
-
-if update_counter != 4:
-    args.reset_dnsmasq = False
-    print("""
-    !!!                         WARNING                         !!!
-    There were {} not 4 snaps updated.   Turning off reset dnsmasq.
-    Double-check the node/snap numbers you provided.
-     - If correct, you'll need to edit the file {} and then run
-       './reset_dnsmasq.sh'
-     - If not, rerun this script with the correct values.
-    """.format(update_counter, args.hosts_file))
+r.hset(rkey, 'ip', rd['ip'])
+r.hset(rkey, 'mac', rd['mac'])
+r.hset(rkey, 'node_ID', args.node_num)
 
 if args.reset_dnsmasq:
     os.system('sudo /etc/init.d/dnsmasq stop')
