f5077246dc77376f650d8066614d4e9bc0e9bd5e
Dave DeBoer <ddeboer@berkeley.edu>
2021-05-09 12:09:04 -0700

    fixed setup for new dir

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except:  # noqa
 
 # Generate a __version__.py file with this version in it
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'monitor-control', 'node_control', '__version__.py'), 'w') as fh:
+with open(os.path.join(here, 'node_control', '__version__.py'), 'w') as fh:
     fh.write('__version__ = "%s"' % ver)
 
 hostname = socket.gethostname()
@@ -21,20 +21,20 @@ hostname = socket.gethostname()
 scripts = []
 if hostname in ['hera-node-head', 'hera-mobile', 'DAVIDs-MBP']:
     scripts = [
-                'monitor-control/scripts/hera_node_data_dump.py',
-                'monitor-control/scripts/hera_node_get_status.py',
-                'monitor-control/scripts/hera_node_keep_alive.py',
-                'monitor-control/scripts/hera_node_receiver.py',
-                'monitor-control/scripts/hera_node_serial_dump.py',
-                'monitor-control/scripts/hera_node_serial.py',
-                'monitor-control/scripts/hera_node_power.py',
-                'monitor-control/scripts/hera_service_status.py',
-                'monitor-control/scripts/hera_setup_new_node.py'
+                'scripts/hera_node_data_dump.py',
+                'scripts/hera_node_get_status.py',
+                'scripts/hera_node_keep_alive.py',
+                'scripts/hera_node_receiver.py',
+                'scripts/hera_node_serial_dump.py',
+                'scripts/hera_node_serial.py',
+                'scripts/hera_node_power.py',
+                'scripts/hera_service_status.py',
+                'scripts/hera_setup_new_node.py'
               ]
 elif hostname == 'hera-snap-head':
-    scripts = ['monitor-control/scripts/hera_setup_new_node.py']
+    scripts = ['scripts/hera_setup_new_node.py']
 else:
-    scripts = ['monitor-control/scripts/hera_node_get_status.py']
+    scripts = ['scripts/hera_node_get_status.py']
 
 setup(
     name='node_control',
@@ -45,9 +45,9 @@ setup(
     author_email='ddeboer@berkeley.edu',
     url='https://github.com/hera_team/hera_node_mc.git',
     long_description=open('README.md').read(),
-    package_dir={'node_control': 'monitor-control/node_control'},
+    package_dir={'node_control': 'node_control'},
     packages=['node_control'],
-    # scripts = [glob.glob('monitor-control/scripts/*'),glob.glob('backend/scripts/*')],
+    # scripts = [glob.glob('scripts/*'),glob.glob('backend/scripts/*')],
     scripts=scripts
 )
 
